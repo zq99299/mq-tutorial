9d1ccc7745f590e07800602432c04e57e5c7332d
zq99299 <99299684@qq.com>
2020-06-22 16:00:01 +0800

    仓库地址

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zq99299/mysql-tutorial.git master:gh-pages
+git push -f git@github.com:zq99299/mq-tutorial.git master:gh-pages
 
 cd -
